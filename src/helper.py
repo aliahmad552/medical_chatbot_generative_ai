b37b3dcf26b2df913dbad60798ec87e661d8a3a9
aliahmad552 <frextarr.552@gmail.com>
2026-01-03 00:26:22 +0500

    d

---
+++ src/helper.py
@@ -0,0 +1,55 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from typing import List
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+load_dotenv()
+
+
+def load_pdf_files(data):
+    loader = DirectoryLoader(
+        data,
+        glob = '*.pdf',
+        loader_cls = PyPDFLoader
+    )
+    documents = loader.load()
+    return documents
+
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    """
+    Given a list of Document objects, return a new list of Document objects
+    containing only 'source' in metadata and the original page_content.
+    """
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get('source')
+        minimal_docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata = {'source':src}
+            )
+        )
+    
+    return minimal_docs
+
+
+
+def text_split(filtered_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 20,
+    )
+    texts_chunk = text_splitter.split_documents(filtered_docs)
+    return texts_chunk
+
+
+def download_embeddings():
+    
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embedding = HuggingFaceEmbeddings(
+        model_name = model_name
+    )
+
+    return embedding
