28d264ef589f4f7fd6d3f2a8654a976385649f54
aliahmad552 <frextarr.552@gmail.com>
2026-01-03 10:38:37 +0500

    ad

--- src/prompt.py
+++ src/prompt.py
@@ -1,11 +1,17 @@
 from langchain_core.prompts import PromptTemplate
 prompt = PromptTemplate(
-    template ="""
-    You are a helpful assistent.
-    Answer Only from the provedid context.
-    If the context is insufficient, just say you don't know.
-    {context}
-    Question: {question}
-    """,
+    template ="""You are a medical assistant.
+
+Use the provided context to answer the question.
+If the context is insufficient, use your general medical knowledge,
+but clearly mention when the answer is not fully supported by the documents.
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:""",
     input_variables=['context','question']
 )
